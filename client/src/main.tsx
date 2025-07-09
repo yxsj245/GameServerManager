7117c3fdf07020009ae2017db7a321a5797b1eb1
小朱 <10714957+xiao-zhu245@user.noreply.gitee.com>
2025-07-09 09:58:17 +0800

    兼容v7

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -11,7 +11,7 @@ initTheme()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
