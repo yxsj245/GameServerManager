7f4db09ba5454a322a958fe25f326e505920822b
小朱 <10714957+xiao-zhu245@user.noreply.gitee.com>
2025-07-15 17:26:16 +0800

    完善提示词

--- client/src/pages/SettingsPage.tsx
+++ client/src/pages/SettingsPage.tsx
@@ -973,7 +973,7 @@ const SettingsPage: React.FC = () => {
               
               {steamcmdSettings.installMode === 'manual' && pathExists === false && (
                 <p className="text-xs text-red-500 mt-1">
-                  在指定路径下未找到steamcmd.exe或steamcmd.sh文件
+                  在指定路径下未找到steamcmd.exe或steamcmd.sh文件。容器中请填写为/home/steam/steamcmd
                 </p>
               )}
               
