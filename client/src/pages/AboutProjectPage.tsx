954e3ee9447cf6ef967db65eaab697dea8c19fb2
小朱 <10714957+xiao-zhu245@user.noreply.gitee.com>
2025-08-09 14:15:45 +0800

    更改版本号

--- client/src/pages/AboutProjectPage.tsx
+++ client/src/pages/AboutProjectPage.tsx
@@ -254,7 +254,7 @@ const AboutProjectPage: React.FC = () => {
                     </div>
                     <div className="flex justify-between">
                       <span className="text-gray-600 dark:text-gray-400">版本：</span>
-                      <span className="text-black dark:text-white font-medium">3.5.2</span>
+                      <span className="text-black dark:text-white font-medium">3.7.0</span>
                     </div>
                     <div className="flex justify-between">
                       <span className="text-gray-600 dark:text-gray-400">开发者：</span>
